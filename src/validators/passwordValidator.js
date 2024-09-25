2f79243673ec0407c1de7d5f9ec63079430b7a57
berrylradianh <berrylhamesha@gmail.com>
2024-09-26 05:59:09 +0700

    fix: condition validator based on unit test

--- src/validators/passwordValidator.js
+++ src/validators/passwordValidator.js
@@ -2,11 +2,11 @@ function passwordValidator(password) {
     const minLength = 15;
 
     if (typeof password !== 'string' || !password) {
-        throw new Error('Password must be a non-empty string');
+        throw new Error('Password must be a non-empty string.');
     }
 
     if (password.length < minLength) {
-        throw new Error(`Password must be at least ${minLength} characters long`);
+        throw new Error(`Password must be at least ${minLength} characters long.`);
     }
 
     const hasUppercase = /[A-Z]/.test(password);
@@ -14,11 +14,10 @@ function passwordValidator(password) {
     const hasDigits = /[0-9]/.test(password);
     const hasSpecialChars = /[!@#$%^&*(),.?":{}|<>]/.test(password);
 
-    const categoryCount = [hasUppercase, hasLowercase, hasDigits, hasSpecialChars]
-        .filter(Boolean).length;
+    const categoryCount = [hasUppercase, hasLowercase, hasDigits, hasSpecialChars].filter(Boolean).length;
 
     if (categoryCount < 2) {
-        throw new Error('Password must include characters from at least 2 categories');
+        throw new Error('Password must include characters from at least 2 of the following categories: uppercase letters, lowercase letters, digits, or special characters.');
     }
 
     return true;
